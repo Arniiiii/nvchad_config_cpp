ec176dbea5337fc026d4c22c6cf296bdbd35d250
Arniiiii <lg3dx6fd@gmail.com>
2025-07-05 10:19:05 +0000

    add fbs related stuff

---
+++ lua/compiler/fbs.lua
@@ -0,0 +1,2 @@
+vim.opt.makeprg = "flatc --conform %" -- Replace with your actual command
+vim.opt.errorformat = "  %f:%l: %c: %m" -- Replace with your errorformat
--- lua/myinit.lua
+++ lua/myinit.lua
@@ -18,3 +18,33 @@ end
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+vim.filetype.add {
+  extension = {
+    fbs = "fbs", -- Map the '.fbs' extension directly to the 'fbs' filetype
+  },
+}
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "fbs", -- The specific filetype you're interested in
+  callback = function(args)
+    -- 'args' contains information about the triggered autocommand
+    -- args.buf is the buffer number
+    -- args.file is the file name
+    -- args.match is the pattern that matched (which will be 'your_filetype_name' here)
+    -- Perform actions specific to this filetype
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.expandtab = true
+
+    -- You could also set makeprg and errorformat here,
+    -- though ftplugin is generally preferred for this.
+    -- vim.opt_local.makeprg = 'my_compiler_for_' .. args.match .. ' %'
+    -- vim.opt_local.errorformat = '%f:%l:%c: %t%*[^:]:%m'
+
+    require "compiler/fbs"
+
+    -- Define buffer-local mappings for this filetype
+    vim.keymap.set("n", "<leader>mm", ":make<CR>", { buffer = args.buf, desc = "Compile current file" })
+  end,
+  desc = "Apply settings for your_filetype_name files",
+})
