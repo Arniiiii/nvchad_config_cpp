ff3f4d230c92751eccd542a1ce4c93fddb521875
Arniiiii <lg3dx6fd@gmail.com>
2025-07-16 09:06:33 +0000

    add nvim-surround

--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -262,6 +262,17 @@ local plugins = {
          vim.g.vimtex_vien_general_options = "--unique file:@pdf#src:@line@tex"
       end,
    },
+
+   {
+      "kylechui/nvim-surround",
+      version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+         require("nvim-surround").setup {
+            -- Configuration here, or leave empty to use defaults
+         }
+      end,
+   },
 }
 
 return plugins
