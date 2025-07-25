dda857a083138b53bbc8fc1735bd3cd47f4c795b
Arniiiii <lg3dx6fd@gmail.com>
2025-07-25 22:07:49 +0000

    completely manually deal with plugins: no import from nvchad; fix cmp autoselecting

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -1,57 +1,59 @@
--- Set up nvim-cmp.
+dofile(vim.g.base46_cache .. "cmp")
+
 local cmp = require "cmp"
 
-cmp.setup {
-   snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-         -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-         require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-         -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-         -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-         -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+local options = {
+   completion = { completeopt = "menu,menuone,noselect" },
 
-         -- For `mini.snippets` users:
-         -- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
-         -- insert({ body = args.body }) -- Insert at cursor
-         -- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
-         -- require("cmp.config").set_onetime({ sources = {} })
+   snippet = {
+      expand = function(args)
+         require("luasnip").lsp_expand(args.body)
       end,
    },
-   window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-   },
-   mapping = cmp.mapping.preset.insert {
-      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+
+   -- preselect = cmp.PreselectMode.None,
+   mapping = {
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<C-n>"] = cmp.mapping.select_next_item(),
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ["<C-e>"] = cmp.mapping.close(),
+
+      ["<CR>"] = cmp.mapping.confirm {
+         behavior = cmp.ConfirmBehavior.Insert,
+         select = true,
+      },
+
+      ["<Tab>"] = cmp.mapping(function(fallback)
+         if cmp.visible() then
+            cmp.select_next_item()
+         elseif require("luasnip").expand_or_jumpable() then
+            require("luasnip").expand_or_jump()
+         else
+            fallback()
+         end
+      end, { "i", "s" }),
+
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+         if cmp.visible() then
+            cmp.select_prev_item()
+         elseif require("luasnip").jumpable(-1) then
+            require("luasnip").jump(-1)
+         else
+            fallback()
+         end
+      end, { "i", "s" }),
    },
-   sources = cmp.config.sources({
+
+   sources = {
       { name = "nvim_lsp" },
-      -- { name = "vsnip" }, -- For vsnip users.
-      { name = "luasnip" }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
-   }, {
+      { name = "luasnip" },
       { name = "buffer" },
-   }),
+      { name = "nvim_lua" },
+      { name = "async_path" },
+   },
 }
-
--- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
--- Set configuration for specific filetype.
---[[ cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      { name = 'git' },
-    }, {
-      { name = 'buffer' },
-    })
- })
- require("cmp_git").setup() ]]
---
-
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ "/", "?" }, {
    mapping = cmp.mapping.preset.cmdline(),
@@ -71,4 +73,4 @@ cmp.setup.cmdline(":", {
    matching = { disallow_symbol_nonprefix_matching = false },
 })
 
-return cmp.setup
+return vim.tbl_deep_extend("keep", options, require "nvchad.cmp")
