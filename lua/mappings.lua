9aacb5d930de80b4fcf7e0e97c25d232b7834927
Arniiiii <lg3dx6fd@gmail.com>
2025-07-25 09:40:09 +0000

    redo a lot of mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,361 +1,484 @@
-require "nvchad.mappings"
+-- require "nvchad.mappings"
 
-vim.keymap.set("v", ">", ">gv", { desc = "general indent" })
+local map = vim.keymap.set
 
-vim.keymap.set(
+-- Disable mappings
+-- local nomap = vim.keymap.del
+
+-- nomap("n", "'")
+
+-- map("n","<C-'>", "<C-i>",{desc="general next in jumplist (tmux workaround)"})
+-- tabufline
+map("n", "<leader>b", "<cmd>enew<CR>", { desc = "buffer new" })
+
+map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
+map("i", "<C-e>", "<End>", { desc = "move end of line" })
+map("i", "<C-h>", "<Left>", { desc = "move left" })
+map("i", "<C-l>", "<Right>", { desc = "move right" })
+map("i", "<C-j>", "<Down>", { desc = "move down" })
+map("i", "<C-k>", "<Up>", { desc = "move up" })
+
+map("n", "<A-q>", function()
+   require("nvchad.tabufline").next()
+end, { desc = "buffer goto next" })
+
+map("n", "<A-Q>", function()
+   require("nvchad.tabufline").prev()
+end, { desc = "buffer goto prev" })
+
+map("n", "<leader>x", function()
+   require("nvchad.tabufline").close_buffer()
+end, { desc = "buffer close" })
+
+-- whichkey
+map("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
+
+map("n", "<leader>wk", function()
+   vim.cmd("WhichKey " .. vim.fn.input "WhichKey: ")
+end, { desc = "whichkey query lookup" })
+
+map({ "n", "x" }, "<leader>fm", function()
+   require("conform").format { lsp_fallback = true }
+end, { desc = "general format file" })
+
+map("n", "<leader>ra", "<cmd>set nu!<CR>", { desc = "toggle line number" })
+map("n", "<leader>rr", "<cmd>set rnu!<CR>", { desc = "toggle relative number" })
+map(
+   "n",
+   "<leader>ch",
+   "<cmd>NvCheatsheet<CR>",
+   { desc = "toggle nvcheatsheet" }
+)
+
+map("n", "<C-s>", "<cmd>w<CR>", { desc = "general save file" })
+map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "general copy whole file" })
+
+map("v", ">", ">gv", { desc = "general indent" })
+map("v", "<", "<gv", { desc = "general backdent" })
+
+map(
    "n",
    "<C-h>",
    ":TmuxNavigateLeft<CR><ESC>",
-   { desc = "tmux_navigator Window left" }
+   { desc = "tmux_navigator Window left", noremap = true, silent = true }
 )
-vim.keymap.set(
+map(
    "n",
    "<C-l>",
    ":TmuxNavigateRight<CR><ESC>",
-   { desc = "tmux_navigator Window right" }
+   { desc = "tmux_navigator Window right", noremap = true, silent = true }
 )
-vim.keymap.set(
+map(
    "n",
    "<C-j>",
    ":TmuxNavigateDown<CR><ESC>",
-   { desc = "tmux_navigator Window down" }
+   { desc = "tmux_navigator Window down", noremap = true, silent = true }
 )
-vim.keymap.set(
+map(
    "n",
    "<C-k>",
    ":TmuxNavigateUp<CR><ESC>",
-   { desc = "tmux_navigator Window left" }
+   { desc = "tmux_navigator Window left", noremap = true, silent = true }
 )
-vim.keymap.set(
+map(
    "n",
    "<C-b>",
    ":TmuxNavigatePrevious<CR><ESC>",
-   { desc = "tmux_navigator Window previous" }
+   { desc = "tmux_navigator Window previous", noremap = true, silent = true }
 )
 
-vim.keymap.set("n", "<leader>dr", function()
+map("n", "<leader>dr", function()
    require("dap").restart()
 end, { desc = "dap Restart session" })
-vim.keymap.set("n", "<leader>rb", function()
+map("n", "<leader>rb", function()
    require("dap").clear_breakboints()
 end, { desc = "dap Remove all breakpoints" })
-vim.keymap.set("n", "<leader>dt", function()
+map("n", "<leader>dt", function()
    require("dap").terminate()
 end, { desc = "dap Terminate session" })
-vim.keymap.set("n", "<leader>tu", function()
+map("n", "<leader>tu", function()
    require("dap").up()
 end, { desc = "dap Go up in current stacktrace without stepping" })
-vim.keymap.set("n", "<leader>td", function()
+map("n", "<leader>td", function()
    require("dap").down()
 end, { desc = "dap Go down in current stacktrace without stepping" })
-vim.keymap.set("n", "<leader>rc", function()
+map("n", "<leader>rc", function()
    require("dap").reverse_continue()
 end, {
    desc = "dap Reverse continue. Debugger & debug adapter have to support it",
 })
-vim.keymap.set("n", "<F5>", function()
+map("n", "<F5>", function()
    require("dap").continue()
 end, { desc = "dap Continue" })
-vim.keymap.set("n", "<F10>", function()
+map("n", "<F10>", function()
    require("dap").step_over()
 end, { desc = "dap Step over" })
-vim.keymap.set("n", "<F11>", function()
+map("n", "<F11>", function()
    require("dap").step_into()
 end, { desc = "dap Step into by statement" })
 -- seems to not work with codelldb ?
--- vim.keymap.set("n", "<F9>", function()
---   require("dap").step_into({steppingGranularity = 'instruction'})
--- end, { desc = "dap Step into by instruction" })
-vim.keymap.set("n", "<F12>", function()
+map("n", "<F9>", function()
+   require("dap").step_into { steppingGranularity = "instruction" }
+end, { desc = "dap Step into by instruction" })
+map("n", "<F12>", function()
    require("dap").step_out()
 end, { desc = "dap Step out" })
-vim.keymap.set("n", "<leader>b", function()
+map("n", "<leader>b", function()
    require("dap").toggle_breakpoint()
 end, { desc = "dap Toggle breakpoint" })
-vim.keymap.set("n", "<leader>sb", function()
+map("n", "<leader>sb", function()
    require("dap").set_breakpoint()
 end, { desc = "dap Set breakpoint" })
-vim.keymap.set("n", "<leader>blp", function()
+map("n", "<leader>blp", function()
    require("dap").set_breakpoint(nil, nil, vim.fn.input "Log point message: ")
 end, { desc = "dap Set breakpoint with log point" })
-vim.keymap.set("n", "<leader>do", function()
+map("n", "<leader>do", function()
    require("dap").repl.open()
 end, { desc = "dap Repl open?" })
-vim.keymap.set("n", "<leader>dl", function()
+map("n", "<leader>dl", function()
    require("dap").run_last()
 end, { desc = "dap Run last?" })
 
-vim.keymap.set("n", "<leader>do", function()
+map("n", "<leader>do", function()
    require("dapui").open()
 end, { desc = "dapui Open" })
-vim.keymap.set("n", "<leader>dc", function()
+map("n", "<leader>dc", function()
    require("dapui").close()
 end, { desc = "dapui Close" })
-vim.keymap.set("n", "<leader>de", function()
+map("n", "<leader>de", function()
    require("dapui").eval()
 end, { desc = "dapui Show hover window with evaluating expression" })
-vim.keymap.set({ "n", "v" }, "<leader>dh", function()
+map({ "n", "v" }, "<leader>dh", function()
    require("dap.ui.widgets").hover()
 end, { desc = "dapui hover?" })
-vim.keymap.set({ "n", "v" }, "<leader>dp", function()
+map({ "n", "v" }, "<leader>dp", function()
    require("dap.ui.widgets").preview()
 end, { desc = "dapui preview?" })
--- vim.keymap.set("n", "<leader>df", function()
+-- map("n", "<leader>df", function()
 --    local widgets = require "dap.ui.widgets"
 --    widgets.centered_float(widgets.frames)
 -- end, { desc = "dapui widgets frame float ?" })
--- vim.keymap.set("n", "<leader>ds", function()
+-- map("n", "<leader>ds", function()
 --    local widgets = require "dap.ui.widgets"
 --    widgets.centered_float(widgets.scopes)
 -- end, { desc = "dapui widgets scopes float ?" })
 
-vim.keymap.set(
+map(
    "n",
-   "<leader>;",
+   "<leader>t;",
    ":Telescope commands<CR>",
    { desc = "Telescope commands" }
 )
-vim.keymap.set(
+map(
    "n",
    "<leader>tch",
    ":Telescope command_history<CR>",
    { desc = "Telescope command history" }
 )
 
-vim.keymap.set(
+-- Comment
+map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
+map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
+
+-- nvimtree
+map(
+   "n",
+   "<C-n>",
+   "<cmd>NvimTreeToggle<CR>",
+   { desc = "nvimtree toggle window" }
+)
+map(
+   "n",
+   "<leader>e",
+   "<cmd>NvimTreeFocus<CR>",
+   { desc = "nvimtree focus window" }
+)
+
+map(
+   "n",
+   "<leader>tg",
+   "<cmd>Telescope live_grep<CR>",
+   { desc = "telescope live grep" }
+)
+map(
+   "n",
+   "<leader>tb",
+   "<cmd>Telescope buffers<CR>",
+   { desc = "telescope find buffers" }
+)
+map(
+   "n",
+   "<leader>tt",
+   "<cmd>Telescope help_tags<CR>",
+   { desc = "telescope help page" }
+)
+map(
+   "n",
+   "<leader>tm",
+   "<cmd>Telescope marks<CR>",
+   { desc = "telescope find marks" }
+)
+map(
+   "n",
+   "<leader>to",
+   "<cmd>Telescope oldfiles<CR>",
+   { desc = "telescope find oldfiles" }
+)
+map(
+   "n",
+   "<leader>tz",
+   "<cmd>Telescope current_buffer_fuzzy_find<CR>",
+   { desc = "telescope find in current buffer" }
+)
+map(
+   "n",
+   "<leader>tcm",
+   "<cmd>Telescope git_commits<CR>",
+   { desc = "telescope git commits" }
+)
+map(
+   "n",
+   "<leader>tg",
+   "<cmd>Telescope git_status<CR>",
+   { desc = "telescope git status" }
+)
+map(
+   "n",
+   "<leader>th",
+   "<cmd>Telescope terms<CR>",
+   { desc = "telescope pick hidden term" }
+)
+
+map(
+   "n",
+   "<leader>tf",
+   "<cmd>Telescope find_files<cr>",
+   { desc = "telescope find files" }
+)
+map(
+   "n",
+   "<leader>tF",
+   "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
+   { desc = "telescope find all files" }
+)
+
+map("n", "<leader>TH", function()
+   require("nvchad.themes").open()
+end, { desc = "telescope nvchad themes" })
+
+-- terminal
+map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
+
+-- toggleable
+map({ "n", "t" }, "<A-v>", function()
+   require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm", size = 20 }
+end, { desc = "terminal toggleable vertical term" })
+
+map({ "n", "t" }, "<A-h>", function()
+   require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm", size = 20 }
+end, { desc = "terminal toggleable horizontal term" })
+
+map({ "n", "t" }, "<A-i>", function()
+   require("nvchad.term").toggle { pos = "float", id = "floatTerm", size = 20 }
+end, { desc = "terminal toggle floating term" })
+
+-- whichkey
+map("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
+
+map("n", "<leader>wk", function()
+   vim.cmd("WhichKey " .. vim.fn.input "WhichKey: ")
+end, { desc = "whichkey query lookup" })
+
+map(
    "n",
    "<leader>CG",
    ":CMakeGenerate <CR>",
    { desc = "CMake Configure and generate" }
 )
-vim.keymap.set("n", "<leader>CB", ":CMakeBuild <CR>", { desc = "CMake Build" })
-vim.keymap.set("n", "<leader>CR", ":CMakeRun <CR>", { desc = "CMake Run" })
-vim.keymap.set(
-   "n",
-   "<leader>CK",
-   ":CMakeSelectKit <CR>",
-   { desc = "CMake Select kit" }
-)
-vim.keymap.set(
+map("n", "<leader>CB", ":CMakeBuild <CR>", { desc = "CMake Build" })
+map("n", "<leader>CR", ":CMakeRun <CR>", { desc = "CMake Run" })
+map("n", "<leader>CK", ":CMakeSelectKit <CR>", { desc = "CMake Select kit" })
+map(
    "n",
    "<leader>CS",
    ":CMakeSettings <CR>",
    { desc = "CMake Project settings" }
 )
-vim.keymap.set(
+map(
    "n",
    "<leader>Cd",
    ":CMakeDebug <CR>",
    { desc = "CMake Debug. Wanna add args?`:CMakeTargetSettings`" }
 )
-vim.keymap.set(
+map(
    "n",
    "<leader>Ce",
    ":CMakeCloseExecutor <CR>",
    { desc = "CMake Close executor" }
 )
-vim.keymap.set(
+map(
    "n",
    "<leader>Cr",
    ":CMakeCloseRunner <CR>",
    { desc = "CMake Close runner" }
 )
-vim.keymap.set(
+map(
    "n",
    "<leader>Coe",
    ":CMakeOpenExecutor <CR>",
    { desc = "CMake Open executor" }
 )
-vim.keymap.set(
-   "n",
-   "<leader>Cor",
-   ":CMakeOpenRunner <CR>",
-   { desc = "CMake Open runner" }
-)
-vim.keymap.set(
+map("n", "<leader>Cor", ":CMakeOpenRunner <CR>", { desc = "CMake Open runner" })
+map(
    "n",
    "<leader>Cse",
    ":CMakeStopExecutor <CR>",
    { desc = "CMake Stop current cmake executor's process" }
 )
-vim.keymap.set(
+map(
    "n",
    "<leader>Csr",
    ":CMakeStopRunner <CR>",
    { desc = "CMake Stop current cmake runner's process" }
 )
-vim.keymap.set("n", "<leader>Cc", ":CMakeClean <CR>", { desc = "CMake Clean" })
-vim.keymap.set(
-   "n",
-   "<leader>Cqb",
-   ":CMakeQuickBuild <CR>",
-   { desc = "CMake Quick build" }
-)
-vim.keymap.set(
-   "n",
-   "<leader>Cqr",
-   ":CMakeQuickRun <CR>",
-   { desc = "CMake Quick run" }
-)
-vim.keymap.set(
-   "n",
-   "<leader>Ci",
-   ":CMakeInstall <CR>",
-   { desc = "CMake Install" }
-)
-vim.keymap.set(
-   "n",
-   "<leader>Cl",
-   ":CMakeLaunchArgs <CR>",
-   { desc = "CMake Launch args" }
-)
-vim.keymap.set(
+map("n", "<leader>Cc", ":CMakeClean <CR>", { desc = "CMake Clean" })
+map("n", "<leader>Cqb", ":CMakeQuickBuild <CR>", { desc = "CMake Quick build" })
+map("n", "<leader>Cqr", ":CMakeQuickRun <CR>", { desc = "CMake Quick run" })
+map("n", "<leader>Ci", ":CMakeInstall <CR>", { desc = "CMake Install" })
+map("n", "<leader>Cl", ":CMakeLaunchArgs <CR>", { desc = "CMake Launch args" })
+map(
    "n",
    "<leader>Csp",
    ":CMakeSelectBuildPreset <CR>",
    { desc = "CMake Select build preset" }
 )
-vim.keymap.set(
+map(
    "n",
    "<leader>Cst",
    ":CMakeSelectBuildTarget <CR>",
    { desc = "CMake Select build target" }
 )
-vim.keymap.set(
+map(
    "n",
    "<leader>CsT",
    ":CMakeSelectBuildType <CR>",
    { desc = "CMake Select build variant" }
 )
-vim.keymap.set(
+map(
    "n",
    "<leader>Csc",
    ":CMakeSelectConfigurePreset <CR>",
    { desc = "CMake Select configure preset" }
 )
-vim.keymap.set(
+map(
    "n",
    "<leader>Csl",
    ":CMakeSelectLaunchTarget <CR>",
    { desc = "CMake Select launch target" }
 )
-vim.keymap.set(
+map(
    "n",
    "<leader>Ctf",
    ":CMakeShowTargetFiles <CR>",
    { desc = "CMake Show target file" }
 )
-vim.keymap.set(
+map(
    "n",
    "<leader>Cts",
    ":CMakeTargetSettings <CR>",
    { desc = "CMake Target settings" }
 )
-vim.keymap.set(
+map(
    "n",
    "<leader>Csb",
    ":CMakeSelectBuildDir <CR>",
    { desc = "CMake Select build dir" }
 )
-vim.keymap.set(
-   "n",
-   "<leader>Csw",
-   ":CMakeSelectCwd <CR>",
-   { desc = "CMake Select cwd" }
-)
+map("n", "<leader>Csw", ":CMakeSelectCwd <CR>", { desc = "CMake Select cwd" })
 
-vim.keymap.set("n", "gD", function()
+map("n", "gD", function()
    vim.lsp.buf.declaration()
 end, {
    desc = "LSP declaration",
 })
 
-vim.keymap.set("n", "gd", function()
+map("n", "gd", function()
    vim.lsp.buf.definition()
 end, {
    desc = "LSP definition",
 })
 
-vim.keymap.set("n", "K", function()
+map("n", "K", function()
    vim.lsp.buf.hover()
 end, {
    desc = "LSP hover",
 })
 
-vim.keymap.set("n", "gi", function()
+map("n", "gi", function()
    vim.lsp.buf.implementation()
 end, {
    desc = "LSP implementation",
 })
 
-vim.keymap.set("n", "<leader>lh", function()
+map("n", "<leader>lh", function()
    vim.lsp.buf.signature_help()
 end, { desc = "LSP signature help" })
 
-vim.keymap.set("n", "<leader>D", function()
+map("n", "<leader>D", function()
    vim.lsp.buf.type_definition()
 end, { desc = "LSP definition type" })
 
-vim.keymap.set("n", "<leader>ra", function()
-   require("nvchad.renamer").open()
-end, { desc = "LSP rename using renamer" })
-
-vim.keymap.set("n", "<F2>", function()
+map("n", "<F2>", function()
    vim.lsp.buf.rename()
 end, { desc = "LSP rename" })
 
-vim.keymap.set("n", "<leader>ca", function()
+map("n", "<leader>lr", function()
+   vim.lsp.buf.rename()
+end, { desc = "LSP rename" })
+
+map({ "n", "v" }, "<leader>4", function()
    vim.lsp.buf.code_action()
 end, { desc = "LSP code action" })
 
-vim.keymap.set({ "n", "v" }, "<leader>4", function()
-   vim.lsp.buf.code_action()
-end, { desc = "LSP code action" })
-
-vim.keymap.set("n", "gr", function()
+map("n", "grr", function()
    vim.lsp.buf.references()
 end, { desc = "LSP references" })
 
-vim.keymap.set("n", "<leader>f", function()
+map("n", "<leader>f", function()
    vim.diagnostic.open_float { border = "rounded" }
 end, { desc = "LSP Floating diagnostic" })
 
-vim.keymap.set("n", "[d", function()
+map("n", "[d", function()
    vim.diagnostic.goto_prev { float = { border = "rounded" } }
 end, { desc = "LSP Goto prev" })
 
-vim.keymap.set("n", "]d", function()
+map("n", "]d", function()
    vim.diagnostic.goto_next { float = { border = "rounded" } }
 end, { desc = "LSP Goto next" })
 
-vim.keymap.set("n", "<leader>q", function()
+map("n", "<leader>ld", function()
    vim.diagnostic.setloclist()
 end, { desc = "LSP Diagnostic setloclist" })
 
-vim.keymap.set("n", "<leader>wa", function()
+map("n", "<leader>lwa", function()
    vim.lsp.buf.add_workspace_folder()
 end, { desc = "LSP Add workspace folder" })
 
-vim.keymap.set("n", "<leader>wr", function()
+map("n", "<leader>lwr", function()
    vim.lsp.buf.remove_workspace_folder()
 end, { desc = "LSP Remove workspace folder" })
 
-vim.keymap.set("n", "<leader>wl", function()
+map("n", "<leader>lwl", function()
    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 end, { desc = "LSP List workspace folders" })
 
-vim.keymap.set("n", "<leader>nf", function()
+map("n", "<leader>gd", function()
    require("neogen").generate()
 end, { desc = "Neogen generate smth" })
 
-vim.keymap.set(
-   "n",
-   "<leader>lr",
-   ":LspRestart <CR>",
-   { desc = "LSP restart LSP" }
-)
-vim.keymap.set("n", "<leader>ls", ":LspStart <CR>", { desc = "LSP start LSP" })
-vim.keymap.set("n", "<leader>lS", ":LspStop <CR>", { desc = "LSP stop LSP" })
+map("n", "<leader>LR", ":LspRestart <CR>", { desc = "LSP restart LSP" })
+map("n", "<leader>Ls", ":LspStart <CR>", { desc = "LSP start LSP" })
+map("n", "<leader>LS", ":LspStop <CR>", { desc = "LSP stop LSP" })
 -- more keybinds!
 
 vim.api.nvim_set_keymap("n", "<C-g>", "<cmd>CodeCompanionActions<cr>", {
@@ -386,10 +509,10 @@ vim.api.nvim_set_keymap("v", "ga", "<cmd>CodeCompanionAdd<cr>", {
    silent = true,
 })
 
-vim.keymap.set("n", "<leader>tt", function()
+map("n", "<leader>TT", function()
    require("base46").toggle_theme()
 end, { desc = "Base46 Toggle theme", noremap = true, silent = true })
-vim.keymap.set("n", "<leader>tT", function()
+map("n", "<leader>Tt", function()
    require("base46").toggle_transparency()
 end, { desc = "Base46 Toggle transparency", noremap = true, silent = true })
 
@@ -551,13 +674,12 @@ vim.api.nvim_set_keymap(
 )
 
 -- Define buffer-local mappings for this filetype
-vim.keymap.set(
+map(
    "n",
    "<leader>mm",
    ":make ",
    { desc = "Compile current file", noremap = true }
 )
-
 
 vim.api.nvim_set_keymap(
    "n",
@@ -575,5 +697,9 @@ vim.api.nvim_set_keymap(
    "n",
    "<leader>lt",
    "<cmd>execute 'ltag ' . expand('<cword>') | lopen<cr>",
-   { desc = "Ctags show matching tags in a location list", noremap = true, silent = true }
+   {
+      desc = "Ctags show matching tags in a location list",
+      noremap = true,
+      silent = true,
+   }
 )
